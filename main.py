e57ef50d3ec0709198a5dcf7778859300a3192b7
Abhinav Srivastava <look4abhinav@outlook.in>
2025-10-12 18:56:30 +0530

    Migrated to Pydantic AI

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import io
+import json
 import os
+from pathlib import Path
 
+import pandas as pd
 from dotenv import load_dotenv
-from pydantic_ai import Agent
+from pdf2image import convert_from_path
+from pydantic_ai import Agent, BinaryContent
 from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.openai import OpenAIProvider
+from rich.console import Console
 
-from prompts.model_prompts import SYSTEM_PROMPT
+import prompts.model_prompts as mp
 
 
 def setup_agent() -> Agent:
@@ -23,11 +29,74 @@ def setup_agent() -> Agent:
         settings={"temperature": 0.1},
     )
 
-    agent = Agent(model, system_prompt=SYSTEM_PROMPT)
+    agent = Agent(model, system_prompt=mp.SYSTEM_PROMPT)
     return agent
 
 
+def read_documents(file_path: Path) -> dict:
+    documents: dict = {}
+
+    if not file_path.exists():
+        console.log(f"[bold red] Source directory {file_path.resolve()} is mising.")
+
+    files = sorted(file_path.glob("*.pdf"))
+
+    for file in files:
+        file_contents = []
+
+        for page in convert_from_path(file, dpi=300):
+            image_bytes = io.BytesIO()
+            page.save(image_bytes, format="PNG")
+
+            image = BinaryContent(image_bytes.getvalue(), media_type="image/png")
+            file_contents.append(image)
+
+        documents[file.name] = file_contents
+
+    return documents
+
+
 if __name__ == "__main__":
+    console = Console()
     agent = setup_agent()
-    response = agent.run_sync("Hi")
-    print(response.output)
+    test_files_path = Path.cwd() / "artifacts" / "test_docs"
+    documents = read_documents(test_files_path)
+
+    features = []
+    data = []
+    output_file_path = Path.cwd() / "artifacts" / "extracted_features.csv"
+
+    for i, (document, content) in enumerate(documents.items(), start=1):
+        console.log(f"[bold blue]Processing [bold green]{i}/{len(documents)} [{document}][bold green][/bold blue]")
+        console.log("[bold cyan]\tExtracting text from PDF...[/bold cyan]")
+        pdf_text = agent.run_sync([mp.EXTRACT_TEXT_FROM_PDF, *content])
+
+        # console.log("[bold cyan]\tExtracting document context...[/bold cyan]")
+        # document_context = agent.run_sync(f"{mp.IDENTIFY_DOCUMENT_CONTEXT}\n {pdf_text}")
+
+        console.log("[bold cyan]\tExtracting important features...[/bold cyan]")
+        important_features = agent.run_sync(f"{mp.INDENTIFY_IMPORTANT_FEATURES}\n {pdf_text}")
+
+        console.log("[bold cyan]\tCombining with existing features...[/bold cyan]")
+        combined_features = agent.run_sync(f"{mp.COMBINE_FEATURES}\n{features} \n{important_features} ")
+        features.extend(list(feature.strip() for feature in combined_features.output.split(",")))
+
+        console.log("[bold cyan]\tExtracting features from text...[/bold cyan]")
+        extracted_features = agent.run_sync(f"{mp.EXTRACT_FEATURES}\n FEATURE: {features} \nTEXT: {pdf_text} ")
+        extracted_features = json.loads(extracted_features.output.replace("```", "").replace("json", ""))
+        extracted_features["Source file"] = document
+
+        data.append(extracted_features)
+        console.clear()
+
+    df = pd.DataFrame(data)
+
+    df.replace("N/A", pd.NA)
+    df.dropna(axis="columns", how="all", inplace=True)
+
+    console.log(df)
+
+    # Save the features
+    df.to_csv(output_file_path, index=False)
+
+    console.log("[bold green] Successful! [/bold green]")
--- prompts/model_prompts.py
+++ prompts/model_prompts.py
@@ -1,5 +1,47 @@
 # Model Prompts
 
 SYSTEM_PROMPT = """
-You are a experienced document anaylzer, expert in processing and extracting meaningful insights from documents.  Keep the answers short and concise.
+You are a experienced document anaylzer, expert in processing and extracting meaningful insights from documents.  Keep the answers short and concise in plain simple text.
+"""
+
+EXTRACT_TEXT_FROM_PDF = """
+You are an expert in extracting information from files. Your task is to extract the contents 
+from the following file. Additionally, if the file contains any images, provide a brief description 
+of each image. If there are no images, do not include an image description in your response. 
+Ensure that you only share the extracted text and image descriptions, and nothing else.
+"""
+
+INDENTIFY_DOCUMENT_CONTEXT = """
+You are an expert in analyzing and summarizing documents. Your task is to summarize the main context, purpose, 
+and type of the following document. Additionally, identify the kind of details the document is related to 
+(e.g., financial report, legal document, research paper, etc.). Ensure that your response is concise and 
+provides only the extracted features as a summary.
+
+Now, analyze and summarize the following document
+"""
+
+INDENTIFY_IMPORTANT_FEATURES = """
+You are an expert in document analysis and feature extraction. Your task is to identify the most important 
+features to extract from a document based on its context. Focus on understanding the document's type, purpose, 
+and the kind of details it contains. Omit features that do not make sense and do not provide additional information 
+in parantheseis. Ensure that your response is concise and provides only the extracted features as a comma separate list.
+Do not include any additional text or explanations.
+
+Now, based on the following document text, suggest the most important features to extract as comma separate list.
+"""
+
+COMBINE_FEATURES = """
+You are an expert in analyzing test. Your task is to combine a list of features with another list of features based on its meaning
+and share one comma separated list. When combining the lists, omit the words that mean the same things so that we don't have
+any duplicates in the final list. Do not include any additional text or explanations.
+
+Now, based on the following 2 feature sets, combine the features meaningfully as a single comma separate list.
+"""
+
+EXTRACT_FEATURES = """
+You are an expert in extracting specific information from documents. Your task is to extract the value of a given feature 
+from the document. If the feature is not present, return 'N/A'. Ensure that your response is in JSON format, where the 
+feature name is the key and the extracted value is the value. Do not include any additional text or explanations.
+
+Now, based on the following feature set and the text below, extract the features from the document and return as JSON.
 """
