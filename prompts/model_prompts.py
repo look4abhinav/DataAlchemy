87a3ee487e009ac99d7a9ab248391f227971b294
Abhinav Srivastava <look4abhinav@outlook.in>
2025-10-12 13:31:09 +0530

    Migrating to Pydantic AI

---
+++ prompts/model_prompts.py
@@ -0,0 +1,5 @@
+# Model Prompts
+
+SYSTEM_PROMPT = """
+You are a experienced document anaylzer, expert in processing and extracting meaningful insights from documents.  Keep the answers short and concise.
+"""
